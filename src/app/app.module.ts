8c089c076c224950e4779cb86e565edcb38eee50
okmmy123456 <mrjtraimanee@gmail.com>
2022-08-17 15:05:45 +0700

    3 files modified, 1 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { PurPrListAssignCom2Component } from './pur/pur-pr-list-assign/pur-pr-list-assign-com2/pur-pr-list-assign-com2.component';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent ,PurPrListAssignCom2Component],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/pur/pur-pr-list-assign/pur-pr-list-assign-com2.component.ts
+++ src/app/pur/pur-pr-list-assign/pur-pr-list-assign-com2.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Output, ViewChild } from '@angular/core';
-import { PrhdService } from 'src/app/shared/prhd.service';
-import { PrlnService } from 'src/app/shared/prln.service';
 
+import { PrlnService } from './'
 @Component({
   selector: 'app-pur-pr-list-assign-com2',
   templateUrl: './pur-pr-list-assign-com2.component.html',
@@ -12,13 +11,11 @@ export class PurPrListAssignCom2Component implements OnInit {
   items: Array<any> = [];
   newItem: any = {};
 
-  constructor(public PrhdService: PrhdService,
+  constructor(
     public PrlnService:PrlnService) {}
 
   ngOnInit(): void {
    // this.PrlnService.indexArr = 1
-    this.PrhdService.feedDataSection()
-    this.PrhdService.feedDataSite()
     this.PrlnService.feedDataUnit()
   }
 }
