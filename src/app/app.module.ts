9e23e04ee0dcd2481f87e57bef510bfebf764e96
okmmy123456 <mrjtraimanee@gmail.com>
2022-08-17 15:46:43 +0700

    3 files modified, 6 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { PurPrListAssignCom2Component } from './pur/pur-pr-list-assign/pur-pr-list-assign-com2/pur-pr-list-assign-com2.component';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
+import { ComMainComponent } from './pur/com-main/com-main.component';
+import {ReactiveFormsModule} from '@angular/forms';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ,PurPrListAssignCom2Component],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatCardModule,
+    RouterModule.forRoot([{ path: '', component: ComMainComponent }]),
+    ReactiveFormsModule,
+
+
+  ],
+  declarations: [AppComponent, HelloComponent,ComMainComponent,],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pur/com-main/com-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-com-main',
+  templateUrl: './com-main.component.html',
+  styleUrls: ['./com-main.component.css']
+})
+export class ComMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
